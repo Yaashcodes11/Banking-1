1a264b00f37dd9c6a31f7d848e560142cc2336cc
Yaashcodes11 <yaashvannk@gmail.com>
2024-05-09 17:11:26 +0530

    Add files via upload

---
+++ Account.py
@@ -0,0 +1,9 @@
+class Account:
+    def __init__(self,bal,ac_no,ac_name):
+        self.bal = bal
+        self.acno = ac_no
+        self.acn = ac_name
+    
+    def deposit(x):
+        bal += x
+        return bal
